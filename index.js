0ab565b6427269530a124d09238d23431df519c6
Ben N <pg.benng@gmail.com>
2023-05-05 11:20:19 +0700

    add event handler

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 // Require the necessary discord.js classes
-const {Client, Collection, Events, GatewayIntentBits} = require('discord.js');
+const {Client, Collection, GatewayIntentBits} = require('discord.js');
 
 // Create a new client instance
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
@@ -32,33 +32,18 @@ for (const folder of commandFolders) {
     }
 }
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, () => {
-    console.log('Ready!');
-});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) {
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-    }
-});
+}
 
 // Log in to Discord with your client's token
 client.login(token);
